123ba4198788f42b2ca5bc2f4f05afdafa11da0a
Nilo Velez <nilovelez@gmail.com>
2017-01-04 22:00:05 +0100

    Ready to go!

---
+++ machete-powertools.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Machete PowerTools Activator
+Description: Unleash Awesome Powa!
+Version: 1.0
+Author: Nilo Velez
+Author URI: http://www.nilovelez.com
+License: WTFPL
+License URI: http://www.wtfpl.net/txt/copying/
+*/
+if ( ! defined( 'ABSPATH' ) ) exit;
+define ('MACHETE_POWERTOOLS_INIT','ec5287c45f0e70ec22d52e8bcbeeb640');
