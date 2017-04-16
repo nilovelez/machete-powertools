b528515f5c514c5dc436a57510e0725ccfa14bee
Nilo Velez <nilovelez@gmail.com>
2017-04-16 17:18:32 +0200

    añadido codemirror para el coloreo de sintaxis del editor de codigo

--- machete-powertools.php
+++ machete-powertools.php
@@ -7,6 +7,11 @@ Author: Nilo Velez
 Author URI: http://www.nilovelez.com
 License: WTFPL
 License URI: http://www.wtfpl.net/txt/copying/
+
+
+
 */
 if ( ! defined( 'ABSPATH' ) ) exit;
 define ('MACHETE_POWERTOOLS_INIT','ec5287c45f0e70ec22d52e8bcbeeb640');
+define ('MACHETE_POWERTOOLS_BASE_URL',  plugin_dir_url( __FILE__ ));
+define ('MACHETE_POWERTOOLS_BASE_PATH',  plugin_dir_path( __FILE__ ));
