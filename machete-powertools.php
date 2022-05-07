7357b517975043583356399573d40cf2f6dcad85
Nilo Velez <nilvoelez@gmail.com>
2022-05-07 12:24:24 +0200

    WordPress Code Standards

--- machete-powertools.php
+++ machete-powertools.php
@@ -1,37 +1,48 @@
 <?php
-/*
-Plugin Name: Machete PowerTools
-Plugin URI: https://machetewp.com/powertools/
-Description: Machete PowerTools is an upgrade module targeted to WordPress developers and power users. PowerTools adds new features and improves some of the Machete modules you know and love.
-Version: 3.0
-Author: Nilo Velez
-Author URI: http://www.nilovelez.com
-License: WTFPL
-License URI: http://www.wtfpl.net/txt/copying/
-*/
+/**
+ * Plugin Name: Machete PowerTools
+ * Plugin URI: https://machetewp.com/powertools/
+ * Description: Machete PowerTools is an upgrade module targeted to WordPress developers and power users. PowerTools adds new features and improves some of the Machete modules you know and love.
+ * Version: 4.0
+ * Author: Nilo Velez
+ * Author URI: http://www.nilovelez.com
+ * License: WTFPL
+ * License URI: http://www.wtfpl.net/txt/copying/
 
-if ( ! defined( 'ABSPATH' ) ) exit;
-define ('MACHETE_POWERTOOLS_INIT','ec5287c45f0e70ec22d52e8bcbeeb640');
-define ('MACHETE_POWERTOOLS_VERSION','v3.0');
-define ('MACHETE_POWERTOOLS_BASE_URL',  plugin_dir_url( __FILE__ ));
-define ('MACHETE_POWERTOOLS_BASE_PATH',  plugin_dir_path( __FILE__ ));
+ * @package WordPress
+ * @subpackage Machete-Powertools
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+define( 'MACHETE_POWERTOOLS_INIT', 'ec5287c45f0e70ec22d52e8bcbeeb640' );
+define( 'MACHETE_POWERTOOLS_VERSION', 'v4.0' );
+define( 'MACHETE_POWERTOOLS_BASE_URL', plugin_dir_url( __FILE__ ) );
+define( 'MACHETE_POWERTOOLS_BASE_PATH', plugin_dir_path( __FILE__ ) );
 
-register_activation_hook( __FILE__, function(){
-  add_option( 'machete_activation_welcome', 'pending');
-});
+register_activation_hook(
+	__FILE__,
+	function() {
+		add_option( 'machete_activation_welcome', 'pending' );
+	}
+);
 
-if (! class_exists('machete_extension_tools') ){
-  require ('class-machete-extension-tools.php');
+if ( ! class_exists( 'machete_extension_tools' ) ) {
+	require 'class-machete-extension-tools.php';
 }
 
-add_action('init', function(){
+add_action(
+	'init',
+	function() {
+		global $machete;
+		global $machete_extension_tools;
 
-  global $machete;
-  global $machete_extension_tools;
+		$machete_extension_tools = new machete_extension_tools( 'Machete PowerTools Activator' );
 
-  $machete_extension_tools = new machete_extension_tools('Machete PowerTools Activator');
+		if ( ! $machete_extension_tools->check_machete_install() ) {
+			return;
+		}
 
-  if ( ! $machete_extension_tools->check_machete_install() ) return;
-
-});
+	}
+);
--- uninstall.php
+++ uninstall.php
@@ -1,16 +1,17 @@
 <?php
-
 /**
  *
  * Uninstall script
  *
  * This file contains all the logic required to uninstall the plugin
  *
+ * @package WordPress
+ * @subpackage Machete
  */
 
-
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
+}
 
-// machete powertool options:
-delete_option('machete_powertools_settings');
+// machete powertool options.
+delete_option( 'machete_powertools_settings' );
